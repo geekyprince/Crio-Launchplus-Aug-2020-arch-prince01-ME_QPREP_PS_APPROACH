ca599f6b0f8048dd5f8b58b470baf28b530fb5fc
geekyprince <arch.prince01@gmail.com>
2020-04-22 09:34:08 +0000

    [LocalTest]:[LinkedListRandomReversal]:[python]

--- LinkedListRandomReversal/solution.py
+++ LinkedListRandomReversal/solution.py
@@ -1,7 +1,7 @@
 from ds.List.ListNode import ListNode, createList
 
 class Solution:
-
+    
 
 
 
@@ -32,23 +32,44 @@ class Solution:
 #      Return the result list
 #
 
-    def performRandomReversal(self, head, nums):
-        res = []
-        for i in nums:
-            if(head):
-                k = i
-                temp = []
-                while(head and k):
-                    temp = [head.val] + temp
-                    head = head.next
-                    k-=1
-                res += temp
-            else:
-                break
-        while(head):
-            res+=[head.val]
+
+def reverse_linked_list(self, head):
+        prev = None
+        nxt = head
+        while(nxt):
+            head = nxt
+            nxt = head.next
+            head.next = prev
+            prev = head
+        return head
+
+
+def split_list(self,head,k):
+        list1_head = head 
+        list2_head = None
+        while(k and head):
+            list2_head = head.next
             head = head.next
-        return createList(res)
+        return (list1_head, list2_head)
+
+
+def append_reversed_list_to_result(self,result_head, result_tail, list_head, list_tail):
+        result_tail.next = list_head
+        result_tail = list_tail
+        return (result_head, result_tail)
+
+
+    def performRandomReversal(self, head, nums):
+        result_head = None
+        result_tail = None
+        for k in nums:
+            (list1_head, list2_head) = self.split_list(head,k)
+            list1_tail = list1_head
+            list1_head = self.reverse_linked_list(list1_head)
+            (result_head, result_tail) = self.append_reversed_list_to_result(result_head, result_tail, list1_head, list1_tail)
+            head = list2_head
+        (result_head, result_tail) = self.append_reversed_list_to_result(result_head, result_tail, head, None)
+        return result_head
             
 
 
