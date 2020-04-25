359879db53908d43bb18f013b7630faf89916571
geekyprince <arch.prince01@gmail.com>
2020-04-25 09:58:02 +0000

    [LocalTest]:[SafeSquare]:[python]

--- SafeSquare/solution.py
+++ SafeSquare/solution.py
@@ -31,9 +31,9 @@ class chess:
         for i in range(i,limit - 1,-1):
             if(not jump):
                 if(k and self.mat[i][j] != '.'):
-                    print(i)
                     break
                 k = 1
+            print(i)
             result[i][j] = 0
         return result
                  
