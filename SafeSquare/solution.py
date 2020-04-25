6a11e102e181dbe31539f34c3056b9557f628bb5
geekyprince <arch.prince01@gmail.com>
2020-04-25 10:01:42 +0000

    [LocalTest]:[SafeSquare]:[python]

--- SafeSquare/solution.py
+++ SafeSquare/solution.py
@@ -55,7 +55,6 @@ class chess:
                 if(k and self.mat[i][j] != '.'):
                     break
                 k = 1
-            print(i,j)
             result[i][j] = 0
         return result
     def left(self,i,j,result,limit,jump = True):
