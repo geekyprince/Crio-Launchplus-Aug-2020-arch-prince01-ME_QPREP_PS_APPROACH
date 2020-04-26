3a44f4bca7fd1db7e9af97a182ed6eb0d3a149f0
geekyprince <arch.prince01@gmail.com>
2020-04-26 05:09:22 +0000

    [LocalTest]:[SameersChoice]:[python]

--- SameersChoice/solution.py
+++ SameersChoice/solution.py
@@ -7,6 +7,7 @@ class Solution:
 
     def lucky_number(self,pattern_list, queries):
         pattern_dict = {}
+        print(pattern_list)
         for A in pattern_list:
             pattern_dict[A[0][0]] = A[0][1]
         print(pattern_dict)
