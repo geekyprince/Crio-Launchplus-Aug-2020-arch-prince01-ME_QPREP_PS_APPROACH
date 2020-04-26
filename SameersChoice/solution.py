408ff0812e71d8d864cf4d1404713f340cfea8f2
geekyprince <arch.prince01@gmail.com>
2020-04-26 05:08:12 +0000

    [LocalTest]:[SameersChoice]:[python]

--- SameersChoice/solution.py
+++ SameersChoice/solution.py
@@ -3,7 +3,10 @@ from collections import defaultdict
 
 class Solution:
 
+    def is_valid_plate_no(self,plateno_valid):
 
     def lucky_number(self,pattern_list, queries):
-        print(pattern_list)
-        print(queries)
+        pattern_dict = {}
+        for A in pattern_list:
+            pattern_dict[A[0][0]] = A[0][1]
+        print(pattern_dict)
