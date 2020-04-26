2b31249e1f6be79aa5617176849f4c91eadb9b84
geekyprince <arch.prince01@gmail.com>
2020-04-26 12:46:05 +0000

    [LocalTest]:[SameersChoice]:[python]

--- SameersChoice/solution.py
+++ SameersChoice/solution.py
@@ -56,7 +56,7 @@ class Solution:
                         pattern_dict[A[0]][-1] = [int(A[1][i]),int(A[1][i+2])]
                         i+=2
                 i+=1
-        print(pattern_dict['motorbike'])
+        #print(pattern_dict['motorbike'])
         for A in queries:
             #print(s.replace('a', ''))
             A[0] = A[0].replace('-','')
