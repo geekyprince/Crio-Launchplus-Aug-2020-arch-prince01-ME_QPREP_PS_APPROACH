5187e3ce2c28cf2c879df9741043243ed261d3e5
geekyprince <arch.prince01@gmail.com>
2020-04-26 12:27:05 +0000

    [LocalTest]:[SameersChoice]:[python]

--- SameersChoice/solution.py
+++ SameersChoice/solution.py
@@ -2,11 +2,48 @@ import re
 from collections import defaultdict
 
 class Solution:
+    def check_print_number(self,n,vehicle_type,pattern_dict):
+        number = ''
+        flag = 1  #decide if number is good or bad
+        for i in range(4):
+            if pattern_dict[vehicle_type][i][0] <= int(n) <= pattern_dict[vehicle_type][i][1]: #if 10000 <= number <= 30000:
+                number +=  n
+            else:
+                flag = 0
+                number += str(pattern_dict[vehicle_type][i][0])
+        if(flag):
+            print('Yes','Good')
+            return True
+        else:
+            print('Yes','Bad',number)
+            return False
 
-    #def is_valid_plate_no(self,plateno_valid):
+    def is_valid_plate_no(self,plate_no,state_codes):
+        l = len(plate_no)
+        if(l<8 or l>11):
+            return False
+        try:
+            if plate_no[0:2] not in state_codes:            #checking State code
+                return False
+            for i in plate_no[2:4]:                         #checking District code
+                i = int(i)       #may lead to exception
+                if(i<0 or i>9):
+                    return False 
+            x = l-8
+            if(x>3 or not plate_no[4:4+x].isalpha()):      #checking presence of alphabet one two three or none
+                return False 
+            if(not l[-4:].isnumeric()): #and  l[-4:]!= l[-4:][::-1]):  #checking Palindrome
+                return False
+        except:
+            return False
+        return True
+            
+        
 
     def lucky_number(self,pattern_list, queries):
+        state_codes = ["AP","AS","AR","BR","CG","GA","GJ","HR","HP","JK","JH","KA","KL","MP","MH","ML","MN","MZ","NL","OD","PB","RJ","SK","TN","TS","TR","UK","UP","WB","AN","CH","DD","DL","LA","LD","PY"]
         pattern_dict = {}
+        result = []
         #print(pattern_list)
         for A in pattern_list:
             l = len(A[1])
@@ -22,6 +59,13 @@ class Solution:
         for A in queries:
             #print(s.replace('a', ''))
             A[0] = A[0].replace('-','')
-        print(queries)
+            if(self.is_valid_plate_no(A[0],state_codes)):
+                vehicle_type = A[1]
+                if(self.check_print_number(l[-4:],vehicle_type,pattern_dict)):
+                    result += [A]
+            else:
+                print('No')
+        for A in result:
+            print(*A)
         
         #print(queries)
