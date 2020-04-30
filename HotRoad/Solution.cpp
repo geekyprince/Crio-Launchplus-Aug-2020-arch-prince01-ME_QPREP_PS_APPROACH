c8666af9aa3db1a5f7ead78a08574388d964844f
Arun prasath Nagarajan <arch.prince01@gmail.com>
2020-05-01 01:40:52 +0550

    updated changes - 2020-05-01 01:20:52

---
+++ HotRoad/Solution.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// TODO: CRIO_TASK_MODULE_HOT_ROAD
+// finish this function
+
+int findMaximumNumberOfPeopleAccomodated(vector< pair<string , int> >& broken, vector<pair< string ,pair<int,int > > >& good, int dist)
+{
+}
