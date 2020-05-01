88024dfdbbc8d04db50d000a1c5f06f951f8928e
Arun prasath Nagarajan <arch.prince01@gmail.com>
2020-05-01 11:29:58 +0550

    updated changes - 2020-05-01 11:09:58

---
+++ HotRoad/Solution.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+import java.util.*;
+
+class Solution {
+
+
+    // TODO: CRIO_TASK_MODULE_HOT_ROAD
+    // finish this function
+
+    public static int find_maximum_number_of_people_accomodated(ArrayList<Entity1> broken, ArrayList<Entity2> good , int k) {
+    }
+}
